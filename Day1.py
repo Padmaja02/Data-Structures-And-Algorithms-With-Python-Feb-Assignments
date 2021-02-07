c8ec3b62e828da8dde6d608bb6094a6af81a1bd1
Padmaja02 <padmajaswaminathan@gmail.com>
2021-02-07 15:59:19 +0530

    Assignments for Day 1 (6th Feb 2021)

---
+++ Day1.py
@@ -0,0 +1,12 @@
+# Assignment 1
+a = int(input("Enter the 1st number: "))
+b = int(input("Enter the 2nd number: "))
+print("Sum of a & b : ", (a+b))
+print("Diffrence between a & b : ", (a-b))
+print("Poduct of a & b : ", (a*b))
+print("Quotient of a & b : ", (a/b))
+
+# Assignment 2
+X = int(input("Enter the 1st number: "))
+Y = int(input("Enter the 2nd number: "))
+print("X^Y : ",X**Y)
